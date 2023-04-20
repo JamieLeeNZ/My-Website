032584c2730ef6996dbf45d9d9d7d57a8014a04f
Jamie <eejl773@aucklanduni.ac.nz>
2023-04-20 19:33:13 +1200

    edited

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,6 @@ import pfp from '../public/newpfp.png';
 import design from '../public/design.png';
 import code from '../public/code.png';
 import consoulting from '../public/consulting.png';
-import web1 from '../public/web1.png';
-import web2 from '../public/web2.png';
-import web3 from '../public/web3.png';
-import web4 from '../public/web4.png';
-import web5 from '../public/web5.png';
-import web6 from '../public/web6.png';
 import {useState} from 'react';
 
 
@@ -105,10 +99,6 @@ export default function Home() {
               <a className="mx-5 w-30 px-4 hover:bg-slate-400 bg-transparent border-2 hover:border-slate-400 text-slate-400 hover:text-white py-2 rounded-md" >Link to GitHub Repository</a>
               <a className="mx-5 w-30 px-4 hover:bg-slate-400 bg-transparent border-2 hover:border-slate-400 text-slate-400 hover:text-white py-2 rounded-md" href ="https://eejl773.github.io/21-Card-Trick/">Link to website</a>
             </div>
-            <div className="basis-1/3 flex-1"><Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
-            <div className="basis-1/3 flex-1"><Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
-            <div className="basis-1/3 flex-1"><Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
-            <div className="basis-1/3 flex-1"><Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/></div>
           </div>
         </section>
       </main>
